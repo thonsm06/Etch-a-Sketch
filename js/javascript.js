53457354e229453cfdebdbfcaf509beac033a794
P51 <116685524+thonsm06@users.noreply.github.com>
2022-11-01 03:40:58 -0500

    Added color change when mousedown and mouseenter

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-
+generateGrid();
 function generateGrid(){
     const container = document.querySelector('.container');
     container.style.width = "320px";
@@ -13,33 +13,28 @@ function generateGrid(){
         grid.classList.add('content');
         grid.style.backgroundColor = 'red';
         grid.style.cssText = "background-color: pink; width: 20px; height: 20px; border-style: solid; border-color: black; border-width: 1px;";
-        grid.addEventListener('mouseover', function(){
-            grid.style.backgroundColor = "blue";
-        })
-        container.appendChild(grid);
-        
+        container.appendChild(grid);  
     }
-    console.log(container);
 }
-/*
-function Hover() {
-    const grids = document.querySelectorAll('.content');
-    grids.forEach(grid => grid.addEventListener('mouseover', function () {
-        console.log(grids);
-    }));
 
-    const len = grids.length;
-    for (let i = 0; i < len; i++)
+const grids = document.querySelectorAll('.content')
+grids.forEach(grid => grid.addEventListener('mousedown', function (){
+    grid.style.backgroundColor = 'blue';
+}))
+grids.forEach(grid => grid.addEventListener('mouseenter', function (e) {
+    if (e.buttons)
     {
-        grids[i].addEventListener('mouseover', () => {
-            grids[i].style.backgroundColor = "blue";
-            console.log(grids);
-        })
-            
+        grid.style.backgroundColor = 'blue';
     }
-}
+}));
+
+
+ //call to generate a new grid
+//mouseHover();
+//mouseDown();
+/*
+if mouse is down inside of a square, change the color of that square.
+if mouse is still down and move to another square, change the color of the new square.
+
+
 */
-//Hover();
-
-
-generateGrid();
