974ff0c3b26d7ea48e4aa96df1558525df2c973f
P51 <116685524+thonsm06@users.noreply.github.com>
2022-11-01 05:42:48 -0500

    updated mouse collision

--- js/javascript.js
+++ js/javascript.js
@@ -1,35 +1,55 @@
-generateGrid();
-function generateGrid(){
+
+window.addEventListener('DOMContentLoaded', function () { //initial grid at start
+    generateGrid(10);
+});
+
+function generateGrid(numOfCell){
     const container = document.querySelector('.container');
-    const containerSize = container.style.width;
-    console.log(containerSize);
-    let numOfSide = 60;
-    let cellSize = (500-(numOfSide*2))/numOfSide;
+    const containerStyle = getComputedStyle(container);
+    let containerSize = containerStyle.getPropertyValue('width');
+    containerSize = containerSize.replace(/[a-zA-Z]/g, ''); //strip all letters, keeping numbers for board size
+    container.replaceChildren();
+    let numOfSide = numOfCell;
+    let cellSize = (containerSize-(numOfSide*2))/numOfSide;
     for(let i = 0; i < numOfSide*numOfSide; i++)
     {
         const grid = document.createElement('div');
         grid.classList.add('content');
-        grid.style.cssText = `background-color: white; width: ${cellSize}px; height: ${cellSize}px; border-style: solid; border-color: black; border-width: 1px;`;
+        grid.style.cssText = `background-color: grey; width: ${cellSize}px; height: ${cellSize}px; border-style: solid; border-color: white; border-width: 1px;`;
         container.appendChild(grid);  
     }
 
 }
+const container = document.querySelector('.container');
+container.addEventListener('mousedown', function() {
+    const grids = document.querySelectorAll('.content');
+    grids.forEach(grid => grid.addEventListener('mouseenter', drawing))
+})
+document.addEventListener('mouseup', function(){
+    const grids = document.querySelectorAll('.content');
+    grids.forEach(grid => grid.removeEventListener('mouseenter', drawing))
+})
 
-const grids = document.querySelectorAll('.content')
-grids.forEach(grid => grid.addEventListener('mousedown', function (){
-    grid.style.backgroundColor = 'blue';
-}))
-grids.forEach(grid => grid.addEventListener('mouseenter', function (e) {
-    if (e.buttons)
+function drawing(e){
+    this.style.backgroundColor = 'black';
+}
+
+const changeSize = document.querySelector('button');
+changeSize.addEventListener('click', function () {
+    let numOfCell = prompt("Enter number of cell");
+    let numOfSide;
+    if (numOfCell == null || numOfCell == "")
     {
-        grid.style.backgroundColor = 'blue';
+        return;
     }
-}));
+    else
+    {
+        generateGrid(numOfCell);
+    }
+});
 
 
- //call to generate a new grid
-//mouseHover();
-//mouseDown();
+
 /*
 if mouse is down inside of a square, change the color of that square.
 if mouse is still down and move to another square, change the color of the new square.
