8a086998c414078e2bd4b22478f3e56a8d002bdb
P51 <116685524+thonsm06@users.noreply.github.com>
2022-11-01 02:28:37 -0500

    hover is working at basic level

--- js/javascript.js
+++ js/javascript.js
@@ -6,17 +6,40 @@ function generateGrid(){
     container.style.display = "flex";
     container.style.flexDirection = "row";
     container.style.flexWrap = 'wrap';
-    
-    for(let i = 0; i < 16*16; i++)
+
+    for(let i = 0; i < 256; i++)
     {
         const grid = document.createElement('div');
         grid.classList.add('content');
         grid.style.backgroundColor = 'red';
-        grid.style.cssText = "background-color: pink; width: 20px; height: 20px";
+        grid.style.cssText = "background-color: pink; width: 20px; height: 20px; border-style: solid; border-color: black; border-width: 1px;";
+        grid.addEventListener('mouseover', function(){
+            grid.style.backgroundColor = "blue";
+        })
         container.appendChild(grid);
         
     }
     console.log(container);
 }
+/*
+function Hover() {
+    const grids = document.querySelectorAll('.content');
+    grids.forEach(grid => grid.addEventListener('mouseover', function () {
+        console.log(grids);
+    }));
+
+    const len = grids.length;
+    for (let i = 0; i < len; i++)
+    {
+        grids[i].addEventListener('mouseover', () => {
+            grids[i].style.backgroundColor = "blue";
+            console.log(grids);
+        })
+            
+    }
+}
+*/
+//Hover();
+
 
 generateGrid();
